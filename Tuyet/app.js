f2d1460d3eafb1b76b4d7808a3da686c3b6e8a95
Tuyet <tuyet_nguyen56@hotmail.com>
2019-06-01 10:47:15 -0700

    html

---
+++ Tuyet/app.js
@@ -0,0 +1,32 @@
+function init() {
+  // Grab a reference to the dropdown select element
+  // Change to d3.select table
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/income").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      // Change to append <tr> 
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+
+}
+
+// function optionChanged(newSample) {
+//   // Fetch new data each time a new sample is selected
+//   buildCharts(newSample);
+//   buildMetadata(newSample);
+// }
+
+// Initialize the dashboard
+init();
+
